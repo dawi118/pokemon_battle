c45dd7c3f88846f6e88e40a884dba99c60249e4a
sjmog <s_morgan@me.com>
2019-07-21 17:13:32 +0100

    Attack enemy feature Added feature to decrease HP by one when 'Attack' button pressed

--- spec/features/hello_world_spec.rb
+++ spec/features/hello_world_spec.rb
@@ -1,7 +1,11 @@
-feature 'Test feature' do
-  scenario 'Hello World' do
+feature 'attacking the enemy' do
+  scenario 'Attacking the enemy, HP decreases' do
     visit '/'
-    expect(page).to have_content 'Hello, World'
+    expect(page).to have_content '10'
+    click_on 'Attack'
+    expect(page).to have_content '9'
+    expect(current_path).to eq "/"
   end
 end
+
 
